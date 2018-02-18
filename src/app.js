5901f38106ddfce714747d4d22cc356a33fce85c
Kyle Urban <kmurban94@yahoo.com>
2018-02-18 17:22:09 -0500

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -8,10 +8,9 @@ import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-import { setTimeout } from 'timers';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
